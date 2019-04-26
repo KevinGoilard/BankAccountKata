0562db3ddcf60e3a3d1858db652ba7fe175fc9e3
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-26 15:10:41 +0200

    Initialized project

---
+++ BankAccountKataTest/DepositTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace BankAccountKataTest
+{
+    [TestClass]
+    public class DepositTest
+    {
+        [TestMethod]
+        public void UnitTestingVerification()
+        {
+            true.Should().Be(true);
+        }
+    }
+}
---
+++ BankAccountKataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountKataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountKataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9f62a9b-15df-44dc-9ede-b8ce27996c83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
